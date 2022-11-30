e302cab255a3c0d2c6f71ab11e4ca6571b0f89c5
Fran Vidal <95777316+Haplee@users.noreply.github.com>
2022-11-30 23:19:24 +0100

    Update Scan y Repair.bat

--- Scan y Repair.bat
+++ Scan y Repair.bat
@@ -1,11 +1,7 @@
 @echo off
-@echo Welcome, thanks for using my sript
-PAUSE
-@echo Press the Enter key to perform a scan and restore of system files.
 PAUSE
 DISM.exe /online /cleanup-image /restorehealth
 sfc /scannow
-@echo All Done
 PAUSE
 exit
 
